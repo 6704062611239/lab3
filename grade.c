a278d6485a8318cee48102ffd4afc12f4d59b3ca
6704062611239 <s6704062611239@email.kmutnb.ac.th>
2025-12-16 13:40:58 +0700

    bonus1

--- grade.c
+++ grade.c
@@ -3,22 +3,23 @@ int main()
 {
 	int score;
 	scanf("%d",&score);
+	
 	if(score >= 68){
 		if (score >= 85){
 			printf("A");
 		}
 		else if(score >= 75){
-			printf("B");
+			printf("B\n%d to A",85-score);
 		}
 		else if(score >= 68){
-			printf("C");
+			printf("C\n%d to B",75-score);
 		}
 	}else{
 		if (score >= 55){
-			printf("D");
+			printf("D\n%d to C",68-score);
 		}
 		else {
-			printf("F");
+			printf("F\n%d to D",55-score);
 		}
 	}
 	return 0;
